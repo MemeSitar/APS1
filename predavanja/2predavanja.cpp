1ab33d164fc99945c80b88f134d2af40dff74ab5
MemeSitar <jurij.sitar@mjau.si>
2023-10-13 13:58:19 +0200

    hahah nekaj ne dela

--- predavanja/2predavanja.cpp
+++ predavanja/2predavanja.cpp
@@ -38,22 +38,48 @@ void insertion_sort(vector<int> &sez) {
 
 void bubble_sort(vector<int> &sez) {
     int n=sez.size();
-    bool change=true;
-    while (change) {
-        change = false;
+    for (int j=0; j<n; j++) {
         for (int i=0; i+1<n; i++){
             if (sez[i] > sez[i+1]){
                 swap(sez[i], sez[i+1]);
-                change = true;
             }
         }
     }
 }
 
+vector<int> merge(vector<int> &a, vector<int> &b) {
+    int i=0, j=0;
+    vector<int> c;
+
+    while (i<a.size() || j<b.size()) {
+        if (i<a.size() && j<b.size()) {
+            if (a[i]<b[j]) c.push_back(a[i++]);
+            else c.push_back(b[j++]);
+        } else if (i<a.size()) c.push_back(a[i++]);
+        else c.push_back(b[i++]);
+    }
+    return c;
+}
+
+vector<int> merge_sort(vector<int> &sez) {
+    int n=sez.size();
+    if (n<=1) return sez;
+    vector<int> levo(sez.begin(), sez.begin()+n/2);
+    vector<int> desno(sez.begin()+n/2, sez.end());
+    levo = merge_sort(levo);
+    desno = merge_sort(desno);
+    return merge(levo, desno);
+}
+
 int main() {
-    vector<int> sez = {9,4,2,8,7,5};
-    print(sez);
-    bubble_sort(sez);
-    print(sez);
+    //vector<int> sez = {9,4,2,8,7,5};
+    //print(sez);
+    //sez = merge_sort(sez);
+    //print(sez);
+
+    vector<int> sez;
+    default_random_engine rnd(123);
+    for(int i = 0;i<10000; i++) sez.push_back(rnd()%1000000);
+    sez = merge_sort(sez);
     return 0;
 }
