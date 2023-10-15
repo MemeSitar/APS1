a424b8377bbc08081018b1c1c94da20229d896bf
MemeSitar <jurij.sitar@mjau.si>
2023-10-15 22:46:13 +0200

    se en nedelujoc algoritem

--- vaje/1teden/osvetlitev.cpp
+++ vaje/1teden/osvetlitev.cpp
@@ -4,33 +4,80 @@
 
 using namespace std;
 
+void print(vector<pair<int, int>> &sez){
+    for (pair<int, int> luc : sez){
+        cout << luc.first << " " << luc.second << endl;
+    }
+}
+
 int main() {
-    int koordinataLuci = 0;
-    int svetlost = 0;
-    int stLuci = 0;
-    int dolzina = 0;
+    int stLuci = 0, dolzina = 0;
+    int koordinataLuci = 0, svetlost = 0;
+    int leva = 0, desna = 0;
+    vector<pair<int, int>> intervali;
+    vector<pair<int, int>> luci;
 
     cin >> dolzina >> stLuci;
     int neosvetljene = dolzina;
 
-    vector<bool> ulica(dolzina, 0);
+    // Zapolni celoten array z lucmi in jih uredi.
     for (int i = 0; i < stLuci; i++){
         cin >> koordinataLuci >> svetlost;
-
         if (svetlost == 0)
             continue;
-        
-        for (int i = koordinataLuci - svetlost; i < koordinataLuci + svetlost; i++){
-            if (i >= dolzina)
-                break;
-            if (i < 0)
-                continue;
-            if (ulica[i] == false){
-                ulica[i] = true;
-                neosvetljene--;
+
+        if (luci.size() == 0){
+            luci.push_back(pair<int, int>(koordinataLuci, svetlost));
+        } else if ((luci.end()-1)->first < koordinataLuci){
+            luci.push_back(pair<int, int>(koordinataLuci, svetlost));
+        } else {
+            for (auto iter = luci.begin(); iter != luci.end(); ++iter){
+                if (iter->first == koordinataLuci){
+                    if (iter->second < svetlost){
+                        iter->second = svetlost;
+                    }
+                    break;
+                }
+                if (iter->first > koordinataLuci){
+                    luci.insert(iter, pair<int, int>(koordinataLuci, svetlost));
+                    break;
+                }
+            }
+        }
+    }
+    printf("-------------------\n");
+    print(luci);
+    printf("-------------------\n");
+
+    for (pair<int, int> luc : luci){
+        leva = max(luc.first - luc.second, 0);
+        desna = min(luc.first + luc.second, dolzina - 1);
+        if (intervali.size() == 0){
+            intervali.push_back(pair<int, int>(leva, desna));
+        } else if ((intervali.end()-1)->second < leva){
+            intervali.push_back(pair<int, int>(leva, desna));
+        } else {
+            for (auto iter = intervali.begin(); iter != intervali.end(); ++iter){
+                if (iter->first == leva){
+                    iter->second = max(iter->second, desna);
+                    break;
+                } else if (iter->second == leva) {
+                    iter->second = max(iter->second, desna);
+                    break;
+                } else if (iter->first > leva){
+                    luci.insert(iter, pair<int, int>(leva, desna));
+                    break;
+                }
             }
         }
     }
 
-    cout << neosvetljene << "\n";
+    print(intervali);
+    printf("mjau\n");
+    // Prestej neosvetljene dele
+    for (pair<int, int> interval : intervali){
+        neosvetljene -= interval.second - interval.first;
+    }
+
+    cout << neosvetljene << endl;
 }
