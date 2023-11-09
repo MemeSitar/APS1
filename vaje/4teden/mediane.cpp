3410199d92e2c4ef275f31664fb4f8c55c945d0b
MemeSitar <jurij.sitar@mjau.si>
2023-11-09 19:00:57 +0100

    Mediane... delajo?

--- vaje/4teden/mediane.cpp
+++ vaje/4teden/mediane.cpp
@@ -3,18 +3,44 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <queue>
 using namespace std;
+
+#define SEGS printf("%s:%d\n", __FILE__, __LINE__);
 
 int main() {
     int n, tmp;
+    int mediana = -1;
     vector<int> vec;
     cin >> n;
 
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    priority_queue<int, vector<int>, less<int>> maxHeap;
+
+    // Vse kar je vecje od mediane gre v minheap
+    // vse kar je manjse gre v maxheap
     for (int i = 0; i < n; i++){
         cin >> tmp;
-        vec.push_back(tmp);
-        sort(vec.begin(), vec.end());
-        cout << vec[(vec.size() - 1)/2] << "\n";
+        if (tmp >= mediana){
+            minHeap.push(tmp);
+        } else if (tmp < mediana){
+            maxHeap.push(tmp);
+        }
+        // rebalance
+        if (((int)minHeap.size() - (int)maxHeap.size()) >= 2){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        } else if (((int) maxHeap.size() - (int)minHeap.size()) >= 2){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+        // izbira mediane
+        if (((int)minHeap.size() - (int)maxHeap.size()) >= 1){
+            mediana = minHeap.top();
+        } else if ((int) maxHeap.size() - (int)minHeap.size() >= 0){
+            mediana = maxHeap.top();
+        }
+        cout << mediana << endl;
     }
     return 0;
 }
