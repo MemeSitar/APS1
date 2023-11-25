f33a8d54338e2f52b7be36fee28bd0c6228181a3
MemeSitar <jurij.sitar@mjau.si>
2023-11-25 22:11:36 +0100

    Dodal ogrodje za 7. nalogo

---
+++ vaje/7teden/ujemanje.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define SEGS printf("%s:%d\n", __FILE__, __LINE__);
+
+int print(vector<pair<int, int>> sez){
+    for(pair<int, int> par : sez){
+        if (par.first == -1){
+            cout << par.first << "\n";
+        } else {
+            cout << par.first << " " << par.second << "\n";
+        }
+    }
+    cout.flush();
+}
+
+pair<int, int> ujemanje(string vzorec, string beseda){
+    pair<int, int> rez(0, 0);
+
+    return rez;
+}
+
+int main(){
+    vector<pair<int, int>> rez;
+    int stNizov;
+    string vzorec, beseda;
+
+    for(int i=0; i<stNizov; i++){
+        cin >> vzorec >> beseda;
+        rez.push_back(ujemanje(vzorec, beseda));
+    }
+
+    print(rez);
+}
