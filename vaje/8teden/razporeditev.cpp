960be0cdb72f8900ad5208d9a552f3f45c2587e7
MemeSitar <jurij.sitar@mjau.si>
2023-11-25 23:00:48 +0100

    zgleda naloga z grafom! - imam ogrodje

---
+++ vaje/8teden/razporeditev.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+#define SEGS printf("%s:%d\n", __FILE__, __LINE__);
+
+int main(){
+
+    return 0;
+}
