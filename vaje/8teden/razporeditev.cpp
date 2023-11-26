d0342c7db030f3965e71aaca2263d715846dc63c
MemeSitar <jurij.sitar@mjau.si>
2023-11-26 13:04:02 +0100

    dodal print heheheh

--- vaje/8teden/razporeditev.cpp
+++ vaje/8teden/razporeditev.cpp
@@ -5,7 +5,24 @@
 using namespace std;
 #define SEGS printf("%s:%d\n", __FILE__, __LINE__);
 
+void print(vector<int> sez){
+    for(int stev : sez){
+        cout << stev << "\n";
+    }
+    cout.flush();
+}
+
 int main(){
+    vector<pair<int, int>> graf;
+    vector<int> rez;
+    int stOtrok, stVpisov;
+    int tmp1, tmp2;
+
+    cin >> stOtrok, stVpisov;
+    for(int i=0; i < stVpisov; i++){
+        cin >> tmp1 >> tmp2;
+        graf.push_back(pair(tmp1, tmp2));
+    }
 
     return 0;
 }
