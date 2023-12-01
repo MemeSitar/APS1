4bd2a1d56057a07980376ac8a168148d01c48236
MemeSitar <jurij.sitar@mjau.si>
2023-12-01 15:03:15 +0100

    debuggiranje funkcija

--- vaje/8teden/razporeditev.cpp
+++ vaje/8teden/razporeditev.cpp
@@ -12,6 +12,16 @@ void print(vector<int> sez){
     cout.flush();
 }
 
+void izpisiSosede(vector<vector<int>> &sosedi){
+    for(int i = 0; i < sosedi.size(); i++){
+        cout << i + 1 << " -> ";
+        for(int sosed : sosedi.at(i)){
+            cout << sosed + 1 << ", ";
+        }
+        cout << endl;
+    }
+}
+
 void dodaj(vector<vector<int>> &tabelaSosedov, int a, int b){
     tabelaSosedov[a - 1].push_back(b - 1);
     tabelaSosedov[b - 1].push_back(a - 1);
@@ -21,13 +31,14 @@ int main(){
     vector<int> rez;
     int stOtrok, stVpisov;
     int tmp1, tmp2;
-    cin >> stOtrok, stVpisov;
+    cin >> stOtrok >> stVpisov;
     vector<vector<int>> sosedi(stOtrok);
     vector<int> barve(stOtrok);
     for(int i=0; i < stVpisov; i++){
         cin >> tmp1 >> tmp2;
         dodaj(sosedi, tmp1, tmp2);
     }
+    izpisiSosede(sosedi);
 
     return 0;
 }
