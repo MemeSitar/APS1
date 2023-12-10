3d54850f96fb0ed94b384e1ce45d85d634ef4001
MemeSitar <jurij.sitar@mjau.si>
2023-12-10 22:26:41 +0100

    dela za vse razen test5.. nek segfault?

--- vaje/9teden/drugapot.cpp
+++ vaje/9teden/drugapot.cpp
@@ -39,7 +39,9 @@ int main(){
     }
 
     // poiscemo najkrajso pot, od zadaj naprej.
+    int najkrajsa;
     Dijkstra(graf, 0, dist, prev);
+    najkrajsa = dist.back();
     vector<int> pot;
     int x = prev.size() - 1;
     while(x != 0){
@@ -48,7 +50,49 @@ int main(){
     }
 
     // gremo cez vse povezave po poti, in jih poskusimo odstraniti.
+    int utez = 0;
+    int tmp = __INT_MAX__;
+    bool niDruge = true;
+    for(int i=0;i<pot.size()-1;i++){
+        n1 = pot.at(i);
+        n2 = pot.at(i+1);
 
+        // najprej odstrani to povezavo, pri obeh
+        for(int j=0; j<graf[n1].size(); j++){
+            if(graf[n1][j].first == n2){
+                utez = graf[n1][j].second;
+                graf[n1].erase(graf[n1].begin() + j);
+                break;
+            }
+        }
+        for(int j=0; j<graf[n2].size(); j++){
+            if(graf[n2][j].first == n1){
+                graf[n2].erase(graf[n2].begin() + j);
+                break;
+            }
+        }
+
+        // potem izvedi dijkstra
+        dist.clear();
+        prev.clear();
+        Dijkstra(graf, 0, dist, prev);
+        if(dist.back() == najkrajsa || dist.back() == najkrajsa + 1){
+            cout << dist.back() << endl;
+            exit(0);
+        } else if(dist.back() != -1 && dist.back() < tmp){
+            niDruge = false;
+            tmp = dist.back();
+        }
+
+        // potem dodaj to povezavo nazaj in pojdi na naslednjo.
+        graf[n1].push_back({n2, utez});
+        graf[n2].push_back({n1, utez});
+    }
+    if(niDruge){
+        cout << "-1" << endl;
+    } else {
+        cout << tmp << endl;
+    }
 
 
     return 0;
